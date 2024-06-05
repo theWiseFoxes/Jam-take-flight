a147b2983b679d3aa5a542c0e880442a72644730
RowanDavitt <rowan.davitt1@ucalgary.ca>
2024-06-05 11:42:30 -0600

    made the gird scale with background better

--- take-flight-game/Scripts/drawGrid.gd
+++ take-flight-game/Scripts/drawGrid.gd
@@ -9,37 +9,38 @@ signal state_updated
 @onready var rows = ObstacleSpawningData.gridRows
 @onready var columns = ObstacleSpawningData.gridColumns
 @onready var cellWidth: float = (screen.size.x / columns) * %tempBackground.scale.x
-@onready var cellHeight: float = (screen.size.y / rows) * %tempBackground.scale.x
+@onready var cellHeight: float = (screen.size.y / rows) * %tempBackground.scale.y
  
 func _draw():
-	screen = %tempBackground.get_rect()
-	var start = Vector2(0, 0)
-	var end = Vector2(0, screen.size.y)
+    var start = %tempBackground.position
+    var end = start + Vector2(0, screen.size.y * %tempBackground.scale.y)
 
-	cellWidth = (screen.size.x / columns) * %tempBackground.scale.x
-	cellHeight = (screen.size.y / rows) * %tempBackground.scale.x
-	var cellInc = Vector2(cellWidth, 0)
-	for rowNum in range(columns + 1):
+    var cellInc = Vector2(cellWidth, 0)
+    for rowNum in range(columns + 1):
 
-		draw_line(start, end, lineColor, 3.0)
-		start += cellInc
-		end += cellInc
-	
-	start = Vector2(0, 0)
-	end = Vector2(screen.size.x, 0)
-	cellInc = Vector2(0, cellHeight)
+        draw_line(start, end, lineColor, 3.0)
+        start += cellInc
+        end += cellInc
+    
+    start = %tempBackground.position
+    end = start+ Vector2(screen.size.x * %tempBackground.scale.x, 0)
+    cellInc = Vector2(0, cellHeight)
 
-	for colNum in range(rows + 1):
-		draw_line(start, end, lineColor, 3.0)
-		start += cellInc
-		end += cellInc
+    for colNum in range(rows + 1):
+        draw_line(start, end, lineColor, 3.0)
+        start += cellInc
+        end += cellInc
 
 func _unhandled_input(event):
-	if placingObject != null:
-		if event is InputEventMouse:
-			var cellPos = (event.position - Vector2(cellWidth / 2, cellHeight / 2)).snapped(Vector2(cellWidth, cellHeight))
-			print(cellPos)
-			placingObject.position = cellPos
-			if event.button_mask&MOUSE_BUTTON_MASK_LEFT != 0:
-				placingObject = null
-				state_updated.emit()
+    if placingObject != null:
+        if event is InputEventMouse:
+            var cellPos = grid_snap(event.position)
+            print(cellPos)
+            placingObject.position = cellPos
+            if event.button_mask&MOUSE_BUTTON_MASK_LEFT != 0:
+                placingObject = null
+                state_updated.emit()
+
+func grid_snap(mousePos):
+    var gridPosition = mousePos - (%tempBackground.position + Vector2(cellWidth / 2, cellHeight / 2))
+    return %tempBackground.position + (gridPosition).snapped(Vector2(cellWidth, cellHeight))
